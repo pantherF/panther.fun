0d43a9548854298a3f6f005bbfa5491ccf6f438c
pantherF <dev@panther.fun>
2026-02-25 13:14:01 +0100

    DONE: auto-pusher now amends to a file that was generated for a given date and commits those changes. This prevents generation of a dozen files for no reason, just to commit some changes
    REMOVED: Removed previous files generated

--- auto-pusher.sh
+++ auto-pusher.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+date="$1"
+
+if [ -z "$1" ]; then
+    now=$(date +'%Y-%m-%d')
+    echo "WARN: No date provided. Defaulting to today: $now"
+    date=$now
+fi
+
+echo $date
+
 random_words () {
     random=$(shuf -i 10-27 -n 1)
     response=$(curl -s "https://random-word-api.herokuapp.com/word?number=$random")
@@ -25,40 +35,38 @@ random_words () {
 }
 
 git_commands () {
-    git add $1
-    
-    CUSTOM_DATE="2026-02-23T12:31:10+01:00"
+    git status
 
+    CUSTOM_DATE="${date}T12:31:10+01:00"
+    
     export GIT_AUTHOR_DATE="$CUSTOM_DATE"
     export GIT_COMMITTER_DATE="$CUSTOM_DATE"
-
-    git add .
-    git commit -m "your message"
+    
+    git add $1
+    
+    git commit -m "Todays contribution: $1"
+    git push -u origin main
     
     unset GIT_AUTHOR_DATE
     unset GIT_COMMITTER_DATE
-
-    git commit -m "Todays contribution: $1"
-    git push -u origin main
 }
 
-generate_and_push () {
-    number=$(($RANDOM%(10-15)+10))
-
-    for ((i=1; i<=number; i++)); do
-        timestamp=$(date +%s)
-	echo "---"
-	echo "Iteration ($i/$number)"
-	echo "---"
-        echo "$timestamp - $(random_words)" >> $1
-
-    done
+write_word_to_file () {
+    timestamp=$(date +%s)
+    echo "Writing random words to todays file"
+    echo "$timestamp - $(random_words)" >> $1
 }
 
-now=$(date +'%Y-%m-%d')
+touch $date.txt
 
-touch $now.txt
+number=$(($RANDOM% 3 + 1))
 
-generate_and_push $now.txt
+for ((i=1; i<=number; i++)); do
+    echo "---"
+    echo "Iteration ($i/$number)"
+    echo "---"
 
-git_commands "$now.txt"
+    write_word_to_file "$date.txt"
+    git_commands "$date.txt"
+done
+#echo "${now}T12:31:10+01:00"
